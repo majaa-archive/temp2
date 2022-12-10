055b00bb8fbe6abf5b9311ca85ce67abdc3687d0
Josip Kelecic <kelecicj@gmail.com>
2022-12-10 17:38:33 +0100

    a54x-common: Add shim for libhypervintf
    * Needed by camera, we don't use hyper HAL.
    
    Change-Id: I823e9286fe2b8ad2320b83cca0e6f59707c30f5b
    Co-Authored-By: chaptsand <chaptsand@gmail.com>
    Co-Authored-By: pckotzer <thegamethis@outlook.com>
    Signed-off-by: ExtremeXT <extremext@lineageos.org>

---
+++ shims/libhypervintf/libhypervintf.cpp
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string>
+#include <vector>
+
+struct HyPerToken {};
+
+void hyper_acquire(HyPerToken, int, std::vector<int, std::allocator<int>>) {}
+
+void hyper_checkHintExist(int) {}
+
+void hyper_checkResourceExist(int) {}
+
+void hyper_getNewHyPerToken() {}
+
+void hyper_init() {}
+
+void hyper_release(HyPerToken) {}
+
+void hyper_setTokenProcName(HyPerToken*, std::string) {}
