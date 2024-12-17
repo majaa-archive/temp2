4f13d8a87a436de5d4e7a60c9d510f087f6a9a85
santhosh6194 <santhosh6194@gmail.com>
2024-12-17 11:03:21 +0530

    a54x-common: Build samsung doze

--- common.mk
+++ common.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.s5e8835
 
+# DOZE
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
