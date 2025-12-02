dfe73e6b71eed9a7cd5ba6f79186bb2ac6b57005
Tim Zimmermann <tim@linux4.de>
2025-12-02 16:35:27 +0200

    gta4xls-common: Add empty libepicoperator shim
    For libexynoscamera3.
    
    Change-Id: I7281041cbe01cfbe17d218bb7ef4859098abef42
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

---
+++ shims/libepicoperator/epicoperator.c
@@ -0,0 +1,8 @@
+unsigned int createOperator(int param_1 __attribute__((unused))) {
+    return 0;
+}
+
+unsigned int createScenarioOperator(int param_1 __attribute__((unused)),
+        int param_2 __attribute__((unused))) {
+    return 0;
+}
