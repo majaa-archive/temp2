0c9296f5da4b26bcd544cc465d1a64226304382a
TestaMic <mateidiac9@gmail.com>
2026-08-15 21:03:05 +0300

    a54x-common: Switch to 64-bit only.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,13 +25,6 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
-## Architecture (Secondary)
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-
 ## Bluetooth
 BOARD_HAVE_BLUETOOTH_SLSI := true
 
