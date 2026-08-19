6d5dd019f088073a3b4e7dab18146cce695eb50b
TestaMic <mateidiac9@gmail.com>
2026-08-19 09:30:42 +0300

    a54x-common: Add fingperint permissions and props.

--- common.mk
+++ common.mk
@@ -134,6 +134,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.s5e8835
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
